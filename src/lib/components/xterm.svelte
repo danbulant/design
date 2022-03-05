6db46cbf0e84b9c8ad613c90b2a7986ff0112f93
Daniel Bulant <danbulant@gmail.com>
2022-03-05 15:05:04 +0100

    add xterm

---
+++ src/lib/components/xterm.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Terminal } from 'xterm';
+    import "xterm/css/xterm.css";
+
+    const term = new Terminal();
+    var div;
+    export var terminal;
+
+    onMount(() => {
+        term.open(div);
+        terminal = term;
+    });
+</script>
+
+<div bind:this={div}></div>

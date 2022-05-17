c14e6e10743a477579414c901f043929a197e124
Daniel Bulant <danbulant@gmail.com>
2022-05-17 14:40:44 +0200

    improve accessibility for image captions

--- src/lib/components/posts/image.svelte
+++ src/lib/components/posts/image.svelte
@@ -1,18 +1,24 @@
 <script>
-    import Described from "./described.svelte";
-
     export var src;
     export var alt;
     export var title;
 </script>
 
-<Described>
+<figure>
     <img {src} {alt} {title} name="content">
-    <slot />
-</Described>
+    <figcaption>
+        <slot />
+    </figcaption>
+</figure>
 
 <style>
     img {
         border-radius: 5px;
+    }
+    figcaption {
+        text-align: center;
+        color: gray;
+        width: 100%;
+        display: inline-block;
     }
 </style>

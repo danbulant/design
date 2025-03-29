8ef657ec02b7085fecda9b752ce59680b112cfcf
Daniel Bulant <danbulant@gmail.com>
2025-03-29 20:32:02 +0100

    dark mode only

--- src/lib/stores/darkmode.js
+++ src/lib/stores/darkmode.js
@@ -4,10 +4,11 @@ import { writable } from "svelte/store";
 // it works! But isn't very performant - darkmode is switched after loading the page, not very friendly
 // could probably set a cookie and have the class be set in SSR as well, I don't really want to spend the time on that now.
 // TODO: make this use cookies and SSR
-const darkmode = writable(
-    typeof window === "undefined" ? false :
-    (JSON.parse(localStorage.getItem("darkmode")) || window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
-);
+// const darkmode = writable(
+//     typeof window === "undefined" ? false :
+//     (JSON.parse(localStorage.getItem("darkmode")) || window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+// );
+const darkmode = writable(true)
 
 darkmode.subscribe(v => {
     if(typeof window !== "undefined")
